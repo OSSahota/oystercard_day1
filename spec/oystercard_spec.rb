798d89ac078b5bad5e63ea690167eabf5ee4724e
OSSahota <osstechunlimited@gmail.com>
2018-12-03 15:08:56 +0000

    4. Add the balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,14 @@
 require 'oystercard'
+
 describe Oystercard do
+
+  context '#balance' do
+
+    let(:oystercard) {Oystercard.new(100)}
+
+    it 'card has money' do
+      expect(oystercard.balance).to eq 100
+    end
+  end
+
 end
